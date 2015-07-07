eb400baaabfd2afbfa5ca9afced906c9e4907f8c
arthwood <admin@arturbilski.pl>
2015-07-07 21:13:01 +0100

    Simpler field values in factory.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,13 +2,13 @@ require_relative '../../model/user'
 
 FactoryGirl.define do
   factory :user, class: MeteorTracker::User do
-    login 'user1'
+    login 'user'
     password 'asdf1234'
     role 'user'
   end
   
   factory :admin, class: MeteorTracker::User do
-    login 'admin111'
+    login 'admin'
     password 'asdf1234'
     role 'admin'
   end
